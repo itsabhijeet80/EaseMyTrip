d6e305542ddde3861b97e47c2aa72706f22ea063
Abhijeet <itsabhijeet80.@gmail.com>
2025-10-21 11:09:04 +0530

    Fix .vercelignore - include source files needed for build

--- vite.config.ts
+++ vite.config.ts
@@ -20,11 +20,8 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist", "public"),
     emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, "client/index.html"),
-    },
   },
   server: {
     fs: {
